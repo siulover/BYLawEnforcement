730978c6ed38039e361024a2a9130a437fcdacf6
siulover <355450280@qq.com>
2017-06-06 16:41:04 +0800

    test page

---
+++ BYLawEnforcement/Areas/BaseManage/Controllers/TestPageController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BYLawEnforcement.Areas.BaseManage.Controllers
+{
+    public class TestPageController : Controller
+    {
+        // GET: BaseManage/TestPage
+        public ActionResult Index()
+        {
+            return View("Index");
+        }
+    }
+}
