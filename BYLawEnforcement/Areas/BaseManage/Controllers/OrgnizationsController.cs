cf117c7c0e6ab5246c189c459064c9cb1e29e6d0
siulover <355450280@qq.com>
2017-06-07 22:20:20 +0800

    test create page

--- BYLawEnforcement/Areas/BaseManage/Controllers/OrgnizationsController.cs
+++ BYLawEnforcement/Areas/BaseManage/Controllers/OrgnizationsController.cs
@@ -1,129 +1,129 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Data;
-//using System.Data.Entity;
-//using System.Linq;
-//using System.Net;
-//using System.Web;
-//using System.Web.Mvc;
-//using LE.Core;
-//using Model;
-//using BYLawEnforcement.Areas.BaseManage.Models;
-//namespace BYLawEnforcement.Areas.BaseManage.Controllers
-//{
-//    public class OrgnizationsController : Controller
-//    {
-//        private OrgnizationManage db = new OrgnizationManage();
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using LE.Core;
+using Model;
+using BYLawEnforcement.Areas.BaseManage.Models;
+namespace BYLawEnforcement.Areas.BaseManage.Controllers
+{
+    public class OrgnizationsController : Controller
+    {
+        private OrgnizationManage db = new OrgnizationManage();
 
-//        // GET: BaseManage/Orgnizations
-//        public ActionResult Index()
-//        {
+        // GET: BaseManage/Orgnizations
+        public ActionResult Index()
+        {
 
-//            return View(db.Orgnizations.ToList());
-//        }
+            return View(db.FindList());
+        }
 
-//        // GET: BaseManage/Orgnizations/Details/5
-//        public ActionResult Details(int? id)
-//        {
-//            if (id == null)
-//            {
-//                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-//            }
-//            Orgnizations orgnizations = db.Orgnizations.Find(id);
-//            if (orgnizations == null)
-//            {
-//                return HttpNotFound();
-//            }
-//            return View(orgnizations);
-//        }
+        // GET: BaseManage/Orgnizations/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Orgnizations orgnizations = db.Find((int)id);
+            if (orgnizations == null)
+            {
+                return HttpNotFound();
+            }
+            return View(orgnizations);
+        }
 
-//        // GET: BaseManage/Orgnizations/Create
-//        public ActionResult Create()
-//        {
-//            return View();
-//        }
+        // GET: BaseManage/Orgnizations/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
 
-//        // POST: BaseManage/Orgnizations/Create
-//        // 为了防止“过多发布”攻击，请启用要绑定到的特定属性，有关 
-//        // 详细信息，请参阅 http://go.microsoft.com/fwlink/?LinkId=317598。
-//        [HttpPost]
-//        [ValidateAntiForgeryToken]
-//        public ActionResult Create([Bind(Include = "OrgNo,OrgName,OrgDuty,OrgDes,OrgFlag")] Orgnizations orgnizations)
-//        {
-//            if (ModelState.IsValid)
-//            {
-//                db.Orgnizations.Add(orgnizations);
-//                db.SaveChanges();
-//                return RedirectToAction("Index");
-//            }
+        // POST: BaseManage/Orgnizations/Create
+        // 为了防止“过多发布”攻击，请启用要绑定到的特定属性，有关 
+        // 详细信息，请参阅 http://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "OrgNo,OrgName,OrgDuty,OrgDes,OrgFlag")] Orgnizations orgnizations)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Add(orgnizations);
+                db.Repositorys.Save();//.SaveChanges();
+                return RedirectToAction("Index");
+            }
 
-//            return View(orgnizations);
-//        }
+            return View(orgnizations);
+        }
 
-//        // GET: BaseManage/Orgnizations/Edit/5
-//        public ActionResult Edit(int? id)
-//        {
-//            if (id == null)
-//            {
-//                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-//            }
-//            Orgnizations orgnizations = db.Orgnizations.Find(id);
-//            if (orgnizations == null)
-//            {
-//                return HttpNotFound();
-//            }
-//            return View(orgnizations);
-//        }
+        //        // GET: BaseManage/Orgnizations/Edit/5
+        //        public ActionResult Edit(int? id)
+        //        {
+        //            if (id == null)
+        //            {
+        //                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //            }
+        //            Orgnizations orgnizations = db.Orgnizations.Find(id);
+        //            if (orgnizations == null)
+        //            {
+        //                return HttpNotFound();
+        //            }
+        //            return View(orgnizations);
+        //        }
 
-//        // POST: BaseManage/Orgnizations/Edit/5
-//        // 为了防止“过多发布”攻击，请启用要绑定到的特定属性，有关 
-//        // 详细信息，请参阅 http://go.microsoft.com/fwlink/?LinkId=317598。
-//        [HttpPost]
-//        [ValidateAntiForgeryToken]
-//        public ActionResult Edit([Bind(Include = "OrgNo,OrgName,OrgDuty,OrgDes,OrgFlag")] Orgnizations orgnizations)
-//        {
-//            if (ModelState.IsValid)
-//            {
-//                db.Entry(orgnizations).State = EntityState.Modified;
-//                db.SaveChanges();
-//                return RedirectToAction("Index");
-//            }
-//            return View(orgnizations);
-//        }
+        //        // POST: BaseManage/Orgnizations/Edit/5
+        //        // 为了防止“过多发布”攻击，请启用要绑定到的特定属性，有关 
+        //        // 详细信息，请参阅 http://go.microsoft.com/fwlink/?LinkId=317598。
+        //        [HttpPost]
+        //        [ValidateAntiForgeryToken]
+        //        public ActionResult Edit([Bind(Include = "OrgNo,OrgName,OrgDuty,OrgDes,OrgFlag")] Orgnizations orgnizations)
+        //        {
+        //            if (ModelState.IsValid)
+        //            {
+        //                db.Entry(orgnizations).State = EntityState.Modified;
+        //                db.SaveChanges();
+        //                return RedirectToAction("Index");
+        //            }
+        //            return View(orgnizations);
+        //        }
 
-//        // GET: BaseManage/Orgnizations/Delete/5
-//        public ActionResult Delete(int? id)
-//        {
-//            if (id == null)
-//            {
-//                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-//            }
-//            Orgnizations orgnizations = db.Orgnizations.Find(id);
-//            if (orgnizations == null)
-//            {
-//                return HttpNotFound();
-//            }
-//            return View(orgnizations);
-//        }
+        //        // GET: BaseManage/Orgnizations/Delete/5
+        //        public ActionResult Delete(int? id)
+        //        {
+        //            if (id == null)
+        //            {
+        //                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //            }
+        //            Orgnizations orgnizations = db.Orgnizations.Find(id);
+        //            if (orgnizations == null)
+        //            {
+        //                return HttpNotFound();
+        //            }
+        //            return View(orgnizations);
+        //        }
 
-//        // POST: BaseManage/Orgnizations/Delete/5
-//        [HttpPost, ActionName("Delete")]
-//        [ValidateAntiForgeryToken]
-//        public ActionResult DeleteConfirmed(int id)
-//        {
-//            Orgnizations orgnizations = db.Orgnizations.Find(id);
-//            db.Orgnizations.Remove(orgnizations);
-//            db.SaveChanges();
-//            return RedirectToAction("Index");
-//        }
+        //        // POST: BaseManage/Orgnizations/Delete/5
+        //        [HttpPost, ActionName("Delete")]
+        //        [ValidateAntiForgeryToken]
+        //        public ActionResult DeleteConfirmed(int id)
+        //        {
+        //            Orgnizations orgnizations = db.Orgnizations.Find(id);
+        //            db.Orgnizations.Remove(orgnizations);
+        //            db.SaveChanges();
+        //            return RedirectToAction("Index");
+        //        }
 
-//        protected override void Dispose(bool disposing)
-//        {
-//            if (disposing)
-//            {
-//                db.Dispose();
-//            }
-//            base.Dispose(disposing);
-//        }
-//    }
-//}
+        //        protected override void Dispose(bool disposing)
+        //        {
+        //            if (disposing)
+        //            {
+        //                db.Dispose();
+        //            }
+        //            base.Dispose(disposing);
+        //        }
+    }
+}
